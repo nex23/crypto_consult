fa4b4953f9a0c57d4640ccefe3f6e08eaf3d322b
Hugo Acosta <thenex@gmail.com>
2023-09-07 17:45:51 -0400

    agregando lectura de input y manejando errores

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+  let mut moneda: String = String::new();
+  let result = std::io::stdin().read_line(&mut moneda);
+  match result {
+    Ok(valor) => println!("Nro. de bytes leídos {valor}"),
+    Err(error) => println!("Epa! ocurrió un Error: {}", error),
+  }
 }
