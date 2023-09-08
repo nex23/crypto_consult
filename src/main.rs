00dd1ecc1fdaa9c4986c6ea27de4d3b2d240fea2
Hugo Acosta <thenex@gmail.com>
2023-09-08 17:48:05 -0400

    adding fetch with ureq crate

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
   let mut moneda: String = String::new();
-  let result = std::io::stdin().read_line(&mut moneda);
-  match result {
-    Ok(_) => {
-      let precio: String = get_precio(&moneda);
-      println!("Precio: {precio}");
-    }
-    Err(error) => println!("Epa! ocurrió un Error: {}", error),
-  }
+  let _ = std::io::stdin()
+    .read_line(&mut moneda)
+    .expect("Ha ocurrido un error leyendo el input");
+  let result_precio = get_precio(&moneda);
+      match result_precio {
+          Ok(precio) => println!("El precio es: {}", precio),
+          Err(error) => println!("Error al buscar el precio {}", error),
+      }
 }
 
-fn get_precio(_moneda: &str) -> String {
-  String::from("Probando..")
+fn get_precio(_moneda: &str) -> Result<String, ureq::Error> {
+  let body: String = ureq::get("http://example.com").call()?
+        .into_string()?;
+    Ok(body)
 }
